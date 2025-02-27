a52efbe89c7e4cbe7b5142e7a5eb7cb1f44bbb24
Hawty1 <marius_erpenbach@web.de>
2025-02-27 22:09:39 +0100

    added audiofiles

---
+++ app/drumMachine.jsx
@@ -0,0 +1,18 @@
+import { useEffect,useState } from "react";
+
+const DrumMachine = () =>{
+    return <div id="drum-machine">
+        <div id="display">
+
+        </div>
+        <div id="pad1" className="drum-pad">Q </div>
+        <div id="pad2" className="drum-pad">W </div>
+        <div id="pad3" className="drum-pad">E </div>
+        <div id="pad4" className="drum-pad">A </div>
+        <div id="pad5" className="drum-pad">S </div>
+        <div id="pad6" className="drum-pad">D </div>
+        <div id="pad7" className="drum-pad">Z </div>
+        <div id="pad8" className="drum-pad">X </div>
+        <div id="pad9" className="drum-pad">C </div>
+    </div>
+}
